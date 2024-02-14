ff1332774b575f45ed8fb165bd4132a540ee2135
Satyajeet Gaur <the.satyajeetgaur@gmail.com>
2024-02-14 13:24:51 +0530

    oddEven using XOR

---
+++ oddEven.py
@@ -0,0 +1,17 @@
+# odd Even using XOR
+
+def isEven(n):
+    if (n ^ 1) == (n + 1):
+        return True
+    else:
+        return False
+    
+
+num = int(input())
+
+if (isEven(num)):
+    print("Even")
+else:
+    print("Odd")
+
+
